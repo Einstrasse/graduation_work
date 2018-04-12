7847f5623df6436c9841d33c2a3dcfb837483686
Einstrasse <hg9587@naver.com>
2018-04-12 05:08:55 -0700

    alarmserver finished feature for list up weekly alarm list

--- iotivity/resource/einstrasse/alarmserver.cpp
+++ iotivity/resource/einstrasse/alarmserver.cpp
@@ -8,17 +8,32 @@
 #include <iostream>
 #include <string>
 #include <sqlite3.h>
-#include <functional>
+
 #include <vector>
 #include "iotivity_config.h"
+
+#include <functional>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#ifdef HAVE_PTHREAD_H
+#include <pthread.h>
+#endif
+#include <mutex>
+#include <condition_variable>
+
 #include "OCPlatform.h"
 #include "OCApi.h"
+#ifdef HAVE_WINDOWS_H
+#include <windows.h>
+#endif
+
+#include "ocpayload.h"
 
 using namespace OC;
 using namespace std;
 namespace PH = std::placeholders;
-
-static const char* PERSISTENT_STORAGE_PATH = "./alarmserver_persistent_storage.dat";
+static const char* SVR_DB_FILE_NAME = "./alarm_svr_db.dat";
 static const char* SQLITE3_ALARM_DB_PATH = "./sqlite3_alarm_db.db";
 
 // Set of strings for each of platform Info fields
@@ -35,7 +50,7 @@ string gSupportLink = "http://eine.tistory.com";
 string gSystemTime = "2018-03-24T11.01";
 
 // Set of strings for each of device info fields
-string  deviceName = "IoTivity Light Server";
+string  deviceName = "IoTivity Alarm Server";
 string  deviceType = "oic.wk.d";
 string  specVersion = "ocf.1.1.0";
 vector<string> dataModelVersions = {"ocf.res.1.1.0", "ocf.sh.1.1.0"};
@@ -44,9 +59,135 @@ string  protocolIndependentID = "fa008167-3bbf-4c9d-8604-c9bcb96cb712";
 // OCPlatformInfo Contains all the platform info to be stored
 OCPlatformInfo platformInfo;
 
-class WeeklyAlarm;
-static int sqliteExecCallback(void* arg, int num_col, char** col_val, char** col_name);
+void PrintIntro() {
+    cout << endl;
+    cout << "------ IoTivity Alarm Server -----\n";
+    cout << "Manipulate Alarm Schedules\n";
+    cout << "Supported Method: GET / POST / PUT / DELETE\n";
+    cout << "1. GET\t- Fetch scheduled alarm data\n";
+    cout << "2. POST\t- Add new scheduled alarm\n";
+    cout << "3. PUT\t- Modify Alarm schedule\n";
+    cout << "4. DELETE\t- Delete previously set Alarm schedule\n";
+}
 
+void DeletePlatformInfo()
+{
+    delete[] platformInfo.platformID;
+    delete[] platformInfo.manufacturerName;
+    delete[] platformInfo.manufacturerUrl;
+    delete[] platformInfo.modelNumber;
+    delete[] platformInfo.dateOfManufacture;
+    delete[] platformInfo.platformVersion;
+    delete[] platformInfo.operatingSystemVersion;
+    delete[] platformInfo.hardwareVersion;
+    delete[] platformInfo.firmwareVersion;
+    delete[] platformInfo.supportUrl;
+    delete[] platformInfo.systemTime;
+}
+
+void DuplicateString(char ** targetString, std::string sourceString) {
+    *targetString = new char[sourceString.length() + 1];
+    strncpy(*targetString, sourceString.c_str(), (sourceString.length() + 1));
+}
+
+OCStackResult SetPlatformInfo(std::string platformID, std::string manufacturerName,
+        std::string manufacturerUrl, std::string modelNumber, std::string dateOfManufacture,
+        std::string platformVersion, std::string operatingSystemVersion,
+        std::string hardwareVersion, std::string firmwareVersion, std::string supportUrl,
+        std::string systemTime) {
+    DuplicateString(&platformInfo.platformID, platformID);
+    DuplicateString(&platformInfo.manufacturerName, manufacturerName);
+    DuplicateString(&platformInfo.manufacturerUrl, manufacturerUrl);
+    DuplicateString(&platformInfo.modelNumber, modelNumber);
+    DuplicateString(&platformInfo.dateOfManufacture, dateOfManufacture);
+    DuplicateString(&platformInfo.platformVersion, platformVersion);
+    DuplicateString(&platformInfo.operatingSystemVersion, operatingSystemVersion);
+    DuplicateString(&platformInfo.hardwareVersion, hardwareVersion);
+    DuplicateString(&platformInfo.firmwareVersion, firmwareVersion);
+    DuplicateString(&platformInfo.supportUrl, supportUrl);
+    DuplicateString(&platformInfo.systemTime, systemTime);
+
+    return OC_STACK_OK;
+}
+
+OCStackResult SetDeviceInfo()
+{
+    OCStackResult result = OC_STACK_ERROR;
+
+    OCResourceHandle handle = OCGetResourceHandleAtUri(OC_RSRVD_DEVICE_URI);
+    if (handle == NULL)
+    {
+        cout << "Failed to find resource " << OC_RSRVD_DEVICE_URI << endl;
+        return result;
+    }
+
+    result = OCBindResourceTypeToResource(handle, deviceType.c_str());
+    if (result != OC_STACK_OK)
+    {
+        cout << "Failed to add device type" << endl;
+        return result;
+    }
+
+    result = OCPlatform::setPropertyValue(PAYLOAD_TYPE_DEVICE, OC_RSRVD_DEVICE_NAME, deviceName);
+    if (result != OC_STACK_OK)
+    {
+        cout << "Failed to set device name" << endl;
+        return result;
+    }
+
+    result = OCPlatform::setPropertyValue(PAYLOAD_TYPE_DEVICE, OC_RSRVD_DATA_MODEL_VERSION,
+                                          dataModelVersions);
+    if (result != OC_STACK_OK)
+    {
+        cout << "Failed to set data model versions" << endl;
+        return result;
+    }
+
+    result = OCPlatform::setPropertyValue(PAYLOAD_TYPE_DEVICE, OC_RSRVD_SPEC_VERSION, specVersion);
+    if (result != OC_STACK_OK)
+    {
+        cout << "Failed to set spec version" << endl;
+        return result;
+    }
+
+    result = OCPlatform::setPropertyValue(PAYLOAD_TYPE_DEVICE, OC_RSRVD_PROTOCOL_INDEPENDENT_ID,
+                                          protocolIndependentID);
+    if (result != OC_STACK_OK)
+    {
+        cout << "Failed to set piid" << endl;
+        return result;
+    }
+
+    return OC_STACK_OK;
+}
+
+static FILE* client_open(const char* path, const char* mode)
+{
+    char const * filename = path;
+    if (0 == strcmp(path, OC_SECURITY_DB_DAT_FILE_NAME))
+    {
+        filename = SVR_DB_FILE_NAME;
+    }
+    else if (0 == strcmp(path, OC_INTROSPECTION_FILE_NAME))
+    {
+        filename = "lightserver_introspection.dat";
+    }
+    return fopen(filename, mode);
+}
+
+class WeeklyAlarmHandler;
+class WeeklyAlarm;
+class AlarmHandler;
+static int sqliteSelectCallback(void* arg, int num_col, char** col_val, char** col_name);
+
+//Class for Genarlly used for Alarm Data
+class AlarmHandler {
+public:
+	static string serialize(vector<WeeklyAlarm>& weeklyList);
+	
+};
+
+//Weekly Alarm Item Object
 class WeeklyAlarm {
 private:
 	int m_id;
@@ -107,9 +248,15 @@ public:
 		cout << "\tenabled: " << m_enabled << '\n';
 		cout << "--------------------------------\n";
 	}
+	string serialize() {
+		return "{\"id\":" + to_string(m_id) + ",\"name\":\"" + m_name + "\",\"hour\":" + to_string(m_hour)+
+		",\"min\":" + to_string(m_min) + ",\"day\":" + to_string(m_day) + ",\"enabled\":" + (m_enabled ? "true" : "false") +"}";
+	}
 
 
 };
+
+//Actually OIC Resource object. Contains and Handle many Weekly alarm data.
 class WeeklyAlarmHandler {
 private:
 	string m_name;
@@ -122,12 +269,44 @@ private:
     OCEntityHandlerResult entityHandler(shared_ptr<OCResourceRequest> request) {
     	cout << "\tIn Server CPP entity handler:\n";
     	OCEntityHandlerResult ehResult = OC_EH_ERROR;
+    	if (request) {
+    		string requestType = request->getRequestType();
+    		int requestFlag = request->getRequestHandlerFlag();
 
+    		if (requestFlag & RequestHandlerFlag::RequestFlag) {
+    			cout << "\t\trequestFlag : Request\n";
+    			auto pResponse = std::make_shared<OC::OCResourceResponse>();
+                pResponse->setRequestHandle(request->getRequestHandle());
+                pResponse->setResourceHandle(request->getResourceHandle());
+
+                // Check for query params (if any)
+                QueryParamsMap queries = request->getQueryParameters();
+
+                if (!queries.empty()) {
+                    std::cout << "\nQuery processing upto entityHandler" << std::endl;
+                }
+                for (auto it : queries) {
+                    std::cout << "Query key: " << it.first << " value : " << it.second
+                            << std:: endl;
+                }
+
+                // If the request type is GET
+                if(requestType == "GET") {
+                    cout << "\t\t\trequestType : GET\n";
+                    pResponse->setResponseResult(OC_EH_OK);
+                    pResponse->setResourceRepresentation(get());
+                    if(OC_STACK_OK == OCPlatform::sendResponse(pResponse)) {
+                        ehResult = OC_EH_OK;
+                    }
+                }
+    		}
+    	} else {
+    		cout << "Invalid Request" << endl;
+    	}
     	return ehResult;
     }
 
 public:
-    
 
 	WeeklyAlarmHandler()
 		:m_name("Weekly Alarms"), m_alarmCount(0), m_serializedData("{}"), 
@@ -159,16 +338,29 @@ public:
 
 		return result;
 	}
+	static string serialize(vector<WeeklyAlarm>& arr) {
+		string ret = "[";
+		for (size_t i=0; i < arr.size(); i++) {
+			auto& it = arr[i];
+			ret += it.serialize();
+			if ( i < arr.size() - 1) {
+				ret += ",";
+			}
+		}
+		ret += "]";
+		return ret;
+	}
 
 	OCRepresentation get() {
 		sqlite3 *db;
 		char *errMsg = 0;
 		int rc = sqlite3_open(SQLITE3_ALARM_DB_PATH, &db);
-		vector<WeeklyAlarm> alarmList;
+		vector<WeeklyAlarm> weeklyAlarmList;
 
 		if (rc) {
 			fprintf(stderr, "SQLite3 file open failed... :%s\n", SQLITE3_ALARM_DB_PATH);
 			m_resourceRep.setValue("alarmCount", -1);
+			m_resourceRep.setValue("serializedData", "{}");
 			return m_resourceRep;
 		} else {
 			printf("SQLite3 file open success!\n");
@@ -176,40 +368,134 @@ public:
 
 		char *sql = "SELECT `id`, `name`, `hour`, `min`, `day`, `enabled` FROM weekly_alarm;";
 
-		rc = sqlite3_exec(db, sql, sqliteExecCallback, (void*) &alarmList, &errMsg);
+		rc = sqlite3_exec(db, sql, sqliteSelectCallback, (void*) &weeklyAlarmList, &errMsg);
 		printf("Sqlite3 Exec function returned..\n");
 		if (SQLITE_OK != rc) {
 			fprintf(stderr, "Sqlite3 sql query failed\n");
 			sqlite3_free(errMsg);
 			m_resourceRep.setValue("alarmCount", -1);
+			m_resourceRep.setValue("serializedData", "{}");
 			return m_resourceRep;
 		} else {
 			printf("Sqlite query success\n");
 		}
 
 		//debug dump sql queried rows
-		for (size_t i=0; i < alarmList.size(); i++) {
-			alarmList[i].dumpData();
-		}
+		// for (size_t i=0; i < weeklyAlarmList.size(); i++) {
+		// 	auto& item = weeklyAlarmList[i];
+		// 	item.dumpData();
+		// 	cout << '\n' << item.serialize() << '\n';
+		// }
 
-		//TODO : JSON serialization of queried row
+		// cout << "\n\n" << (WeeklyAlarmHandler::serialize(weeklyAlarmList)) << "\n\n";
+		// cout << AlarmHandler::serialize(weeklyAlarmList) << "\n\n";
 
 		sqlite3_close(db);
+
+		m_resourceRep.setValue("alarmCount", (int)weeklyAlarmList.size());
+		m_resourceRep.setValue("serializedData", AlarmHandler::serialize(weeklyAlarmList));
 		return m_resourceRep;
 	}
 
+	void addType(const std::string& type) const {
+        OCStackResult result = OCPlatform::bindTypeToResource(m_resourceHandle, type);
+        if (OC_STACK_OK != result)
+        {
+            cout << "Binding TypeName to Resource was unsuccessful\n";
+        }
+    }
 
+    void addInterface(const std::string& iface) const {
+        OCStackResult result = OCPlatform::bindInterfaceToResource(m_resourceHandle, iface);
+        if (OC_STACK_OK != result)
+        {
+            cout << "Binding TypeName to Resource was unsuccessful\n";
+        }
+    }
 };
 
 int main() {
-	//debug mode
-	WeeklyAlarmHandler weeklyAlarmHandler;
 
-	weeklyAlarmHandler.get();
+	PrintIntro();
+	OCPersistentStorage ps {client_open, fread, fwrite, fclose, unlink };
+    //Platform setting and start
+    PlatformConfig cfg {
+        OC::ServiceType::InProc,
+        OC::ModeType::Server,
+        &ps
+    };
+    cfg.transportType = static_cast<OCTransportAdapter>(OCTransportAdapter::OC_ADAPTER_IP | 
+                                                        OCTransportAdapter::OC_ADAPTER_TCP);
+    cfg.QoS = OC::QualityOfService::LowQos;
+    OCPlatform::Configure(cfg);
+    OC_VERIFY(OCPlatform::start() == OC_STACK_OK);
+    std::cout << "Setting Platform Info finished. Server Starts....\n";
+
+    OCStackResult result = SetPlatformInfo(gPlatformId, gManufacturerName, gManufacturerLink,
+            gModelNumber, gDateOfManufacture, gPlatformVersion, gOperatingSystemVersion,
+            gHardwareVersion, gFirmwareVersion, gSupportLink, gSystemTime);
+
+    result = OCPlatform::registerPlatformInfo(platformInfo);
+
+    if (result != OC_STACK_OK)
+    {
+        std::cout << "Platform Registration failed\n";
+        return -1;
+    } else {
+        std::cout << "Platform Registration succeed\n";
+    }
+
+    result = SetDeviceInfo();
+
+    if (result != OC_STACK_OK)
+    {
+        std::cout << "Device Registration failed\n";
+        return -1;
+    } else {
+        std::cout << "Device Registration succeed\n";
+    }
+
+    try
+    {
+        // Create the instance of the resource class
+        // (in this case instance of class 'LightResource').
+        WeeklyAlarmHandler weeklyAlarmHandler;
+
+        // Invoke createResource function of class light.
+        weeklyAlarmHandler.createResource();
+        std::cout << "Created resource." << std::endl;
+
+        weeklyAlarmHandler.addType(std::string("core.alarm"));
+        weeklyAlarmHandler.addInterface(std::string(LINK_INTERFACE));
+        std::cout << "Added Interface and Type" << std::endl;
+
+        DeletePlatformInfo();
+
+        // A condition variable will free the mutex it is given, then do a non-
+        // intensive block until 'notify' is called on it.  In this case, since we
+        // don't ever call cv.notify, this should be a non-processor intensive version
+        // of while(true);
+        std::mutex blocker;
+        std::condition_variable cv;
+        std::unique_lock<std::mutex> lock(blocker);
+        std::cout <<"Waiting" << std::endl;
+        cv.wait(lock, []{return false;});
+    }
+    catch(OCException &e)
+    {
+        std::cout << "OCException in main : " << e.what() << endl;
+    }
+
+	//debug mode
+	// WeeklyAlarmHandler weeklyAlarmHandler;
+
+	// weeklyAlarmHandler.get();
+
+	OC_VERIFY(OCPlatform::stop() == OC_STACK_OK);
 	return 0;
 }
 
-static int sqliteExecCallback(void* arg, int num_col, char** col_val, char** col_name) {
+static int sqliteSelectCallback(void* arg, int num_col, char** col_val, char** col_name) {
 	vector<WeeklyAlarm> *listPtr = (vector<WeeklyAlarm>*) arg;
 	WeeklyAlarm item;
 	item.setId(atoi(col_val[0]));
@@ -220,4 +506,11 @@ static int sqliteExecCallback(void* arg, int num_col, char** col_val, char** col
 	item.setEnabled(strcmp(col_val[5], "1") == 0 ? true : false);
 	listPtr->push_back(item);
 	return 0;
+}
+
+string AlarmHandler::serialize(vector<WeeklyAlarm>& weeklyList) {
+	string ret = "{\"weekly\":";
+	ret += WeeklyAlarmHandler::serialize(weeklyList);
+	ret += "}";
+	return ret;
 }
